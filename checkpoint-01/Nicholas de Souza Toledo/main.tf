714d97d08dfd3d1e7c7c65a0680f27d5252808cb
Nicholas Toledo <rm88882@fiap.com.br>
2021-09-01 21:43:04 -0300

    checkpoint 3 tentativa

--- checkpoint-01/Nicholas de Souza Toledo/main.tf
+++ checkpoint-01/Nicholas de Souza Toledo/main.tf
@@ -73,16 +73,27 @@ resource "aws_security_group" "Work_Nagios_Security_Group" {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
     ingress {
       from_port       = "0"
       to_port         = "0"
       protocol        = "-1"
-      cidr_blocks = [ "0.0.0.0/0" ]
+      cidr_blocks = [ "10.0.0.0/16" ]
+  }
+  ingress {
+    from_port = 0
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port = 0
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
 
 }
 
