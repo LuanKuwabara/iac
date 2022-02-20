9f0799145d597e225919b2230cee2ccf2dd126a9
Kledson Basso <kledsonhugo@gmail.com>
2022-02-20 17:47:13 -0300

    Delete checkpoint-03/Lucas Linhares Ramos directory

--- checkpoint-03/Lucas Linhares Ramos/modules/ec2/output.tf
+++
@@ -1,7 +0,0 @@
-output "sg_pub_id" {
-  value = "${aws_security_group.sg_pub.id}"
-}
-
-output "sg_priv_id" {
-  value = "${aws_security_group.sg_priv.id}"
-}
--- checkpoint-03/Lucas Linhares Ramos/vars.tf
+++
@@ -1,7 +0,0 @@
-# Variáveis que irei chamar no main
-variable "aws_region" {
-  type = string
-  default = "us-east-1"
-}
-
-variable "sg_priv_id" {}
