d6fb8c7b64fbab1be0e04aed524154d2fa3e5e69
Kledson Basso <kledsonhugo@gmail.com>
2022-03-30 07:40:20 -0300

    Update userdata-nagioscore.sh

--- checkpoint-01/nagios/userdata-nagioscore.sh
+++ checkpoint-01/nagios/userdata-nagioscore.sh
@@ -9,9 +9,9 @@ yum install -y gcc glibc glibc-common make gettext automake autoconf wget openss
 yum install -y perl-Net-SNMP
 yum install -y unzip httpd php gd gd-devel perl postfix
 cd /tmp
-wget -O nagioscore.tar.gz https://github.com/NagiosEnterprises/nagioscore/archive/nagios-4.4.5.tar.gz
+wget -O nagioscore.tar.gz https://github.com/NagiosEnterprises/nagioscore/archive/nagios-4.4.6.tar.gz
 tar xzf nagioscore.tar.gz
-cd /tmp/nagioscore-nagios-4.4.5/
+cd /tmp/nagioscore-nagios-4.4.6/
 ./configure
 make all
 make install-groups-users
@@ -28,9 +28,9 @@ htpasswd -b -c /usr/local/nagios/etc/htpasswd.users nagiosadmin nagiosadmin
 service httpd start
 service nagios start
 cd /tmp
-wget --no-check-certificate -O nagios-plugins.tar.gz https://github.com/nagios-plugins/nagios-plugins/archive/release-2.2.1.tar.gz
+wget --no-check-certificate -O nagios-plugins.tar.gz https://github.com/nagios-plugins/nagios-plugins/archive/release-2.3.3.tar.gz
 tar zxf nagios-plugins.tar.gz
-cd /tmp/nagios-plugins-release-2.2.1/
+cd /tmp/nagios-plugins-release-2.3.3/
 ./tools/setup
 ./configure
 make
